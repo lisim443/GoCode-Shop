f941851e015151c21d24d181099a5680191c4d45
lisim <lior443@gmail.com>
2021-10-20 21:49:58 +0300

    uploading all project files.

---
+++ src/components/Products/Products.js
@@ -0,0 +1,17 @@
+import Product from "../Product/Product";
+import "./Products.css";
+
+function Products() {
+  return (
+    <section class="products">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </section>
+  );
+}
+
+export default Products;
